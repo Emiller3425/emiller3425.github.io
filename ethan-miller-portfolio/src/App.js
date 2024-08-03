c41913d08b1d148d112f6328aa806cf99faa1ec4
Ethan Miller <milleret@mail.gvsu.edu>
2024-08-03 17:21:36 -0400

    package.json

--- ethan-miller-portfolio/src/App.js
+++ ethan-miller-portfolio/src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import ProjectDetail from './components/ProjectDetail';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen bg-lavender">
-      <Header />
-      <div className="container mx-auto p-4 flex-1">
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:id" element={<ProjectDetail />} />
-        </Routes>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:id" element={<ProjectDetail />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
