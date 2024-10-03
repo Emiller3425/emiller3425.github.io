6513ff081c3b8ebf7dabd430a7ac116c9dabdb2c
Ethan Miller <milleret@mail.gvsu.edu>
2024-10-03 10:14:40 -0400

    hover on items

--- ethan-miller-portfolio/src/components/ProjectItem.js
+++ ethan-miller-portfolio/src/components/ProjectItem.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function ProjectItem({ id, imgSrc, title, description }) {
   return (
-    <Link to={`/projects/${id}`} className="project-item bg-gray-800 p-6 mb-6 rounded-lg hover:shadow-xl transition-shadow">
+    <Link to={`/projects/${id}`} className="project-item bg-gray-800 p-6 mb-6 rounded-lg transition-transform transform hover:scale-102">
       <div className="flex flex-col md:flex-row items-center">
         <img src={imgSrc} alt={title} className="project-image w-32 h-32 mb-4 md:mb-0 md:mr-6 border-4 border-gray-700 rounded-lg" />
         <div className="project-info text-gray-200">
